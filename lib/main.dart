3797da48adcb4e78bfa2b0e9f494c28c22c8018d
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2020-01-16 20:05:47 +0900

    setState

--- lib/main.dart
+++ lib/main.dart
@@ -35,18 +35,30 @@ class HelloPage extends StatefulWidget {
 }
 
 class _HelloPageState extends State<HelloPage> {
+  String _message = 'Hello World';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _changeMessage
+        ),
         appBar: AppBar(
-          title: Text(widget.title)
+            title: Text(widget.title)
         ),
         body: Text(
-            widget.title,
-            style: TextStyle(
-                fontSize: 30
-            )
+          _message,
+          style: TextStyle(
+            fontSize: 30
+          )
         )
     );
+  }
+
+  void _changeMessage() {
+    setState(() {
+      _message = 'Clicked!';
+    });
   }
 }
